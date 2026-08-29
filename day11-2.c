e218b520ed8a5e8e2c56659e48e3af1110c80b73
Ashish kumar <305499428+ashishakkumar2007-del@users.noreply.github.com>
2026-08-29 23:50:13 +0530

    Add files via upload

---
+++ day11-2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, profit, loss;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    if (sp > cp)
+    {
+        profit = sp - cp;
+        printf("Profit = %.2f\n", profit);
+        printf("Profit Percentage = %.2f%%", (profit / cp) * 100);
+    }
+    else if (cp > sp)
+    {
+        loss = cp - sp;
+        printf("Loss = %.2f\n", loss);
+        printf("Loss Percentage = %.2f%%", (loss / cp) * 100);
+    }
+    else
+    {
+        printf("No Profit No Loss");
+    }
+
+    return 0;
+}
