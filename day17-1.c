146476fbc9b7dd7636e6a5b3347ef1e0e36296f3
Ashish kumar <305499428+ashishakkumar2007-del@users.noreply.github.com>
2026-09-12 22:40:49 +0530

    Add files via upload

---
+++ day17-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, original, rem, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while(n > 0)
+    {
+        rem = n % 10;
+        sum = sum + rem * rem * rem;
+        n = n / 10;
+    }
+
+    if(original == sum)
+        printf("Armstrong number");
+    else
+        printf("Not an Armstrong number");
+
+    return 0;
+}
