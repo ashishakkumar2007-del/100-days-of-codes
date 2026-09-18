cd8dc37049a48a90dca20f372b3c4c6506df6368
Ashish kumar <305499428+ashishakkumar2007-del@users.noreply.github.com>
2026-09-18 23:25:22 +0530

    Add files via upload

---
+++ day22-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, digit, i, fact, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n > 0)
+    {
+        digit = n % 10;
+        fact = 1;
+
+        for(i = 1; i <= digit; i++)
+        {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        n = n / 10;
+    }
+
+    if(sum == temp)
+    {
+        printf("Strong number");
+    }
+    else
+    {
+        printf("Not a strong number");
+    }
+
+    return 0;
+}
