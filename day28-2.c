ec58a20f3ee263c57593a1abc4d8cae666ef1555
Ashish kumar <305499428+ashishakkumar2007-del@users.noreply.github.com>
2026-09-26 16:18:23 +0530

    Add files via upload

---
+++ day28-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Array elements are: ");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
